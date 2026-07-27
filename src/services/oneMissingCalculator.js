adc20ba66ab29159bf4493761e2749f555dd61f3
__Dany__ <tolozadaniel5@gmail.com>
2026-07-27 04:12:21 -0400

    refactor: optimize weight calculation in oneMissingCalculator function

--- src/services/oneMissingCalculator.js
+++ src/services/oneMissingCalculator.js
@@ -1,24 +1,28 @@
 export function oneMissingCalculator({evaluations, targetGrade, configGrade}) {
-    const totalWeight = evaluations.reduce(
-      (sum, evaluation) => sum + evaluation.weight,
-      0
-    );
+    let totalWeight = 0
 
     let evalIncognita =null
     const otherEvals= new Array()
     for (const evaluation of evaluations){
-        if (evaluation.grade === null) evalIncognita = evaluation
-        else otherEvals.push(evaluation)
-
+        // formatear bien lo porcentajes para el calculo
+        const evaluationFormated = {
+            ...evaluation,
+            weight:evaluation.weight*0.01
+        }
+        //sacar total de los porcentajes
+        totalWeight += evaluationFormated.weight
+        if (evaluation.grade === null) evalIncognita = evaluationFormated
+        else otherEvals.push(evaluationFormated)
+        
     }
     console.log(evalIncognita)
-    console.log('papopepos')
+    console.log('---------------')
     console.log(otherEvals)
     let numerador=4
     for (const evaluation of otherEvals){
-        numerador = numerador-(evaluation.grade*(evaluation.weight*0.01)/(totalWeight*0.01))
+        numerador = numerador-(evaluation.grade*evaluation.weight/totalWeight)
     }
-    const result=numerador/((evalIncognita.weight*0.01)/(totalWeight*0.01))
+    const result=numerador/((evalIncognita.weight)/(totalWeight))
     console.log(result)
 
 }
@@ -27,9 +31,17 @@ export function oneMissingCalculator({evaluations, targetGrade, configGrade}) {
 
 //oneMissingCalculator({ 
 //    evaluations:[
-//        {name:"hola", grade:1, weight:30},
-//        {name:"hola", grade:7, weight:20},
+//        {name:"hola", grade:1, weight:20},
+//        {name:"hola", grade:null, weight:20},
 //
-//        {name:"hola2", grade:null, weight:10}
+//        
+//    ]
+//})
+
+//oneMissingCalculator({ 
+//    evaluations:[
+//        {name:"hola", grade:1, weight:20},
+//        {name:"hola2", grade:7, weight:30},
+//        {name:"hola3", grade:null, weight:50},    
 //    ]
 //})
