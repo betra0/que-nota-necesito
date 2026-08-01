2c49c2ed2eb9d04b2becdda174c449d40dd68da2
__Dany__ <tolozadaniel5@gmail.com>
2026-07-31 23:35:31 -0400

    feat: restructure App component layout and add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -6,55 +6,50 @@ import './App.css'
 import HomeCard from './components/HomeCard'
 //import { IconBrandGithub } from '@tabler/icons-react';
 import githubLogo from './assets/github.svg'
-
+import Footer from './components/Footer'
 function App() {
 
   return (
-    <div className="
-      min-h-screen
-      bg-gray-950
-      bg-[radial-gradient(circle_at_15%_15%,rgba(139,92,246,0.22),transparent_30%),radial-gradient(circle_at_85%_85%,rgba(59,130,246,0.18),transparent_30%)]
-      text-white
-    ">
+  <div className="relative min-h-screen text-white">
 
+    {/* Fondo */}
+    <div
+      className="
+        fixed
+        inset-0
+        min-h-full
+        z-0
+        pointer-events-none
+        bg-gray-950
+        bg-[radial-gradient(circle_at_15%_15%,rgba(139,92,246,0.22),transparent_30%),radial-gradient(circle_at_85%_85%,rgba(59,130,246,0.18),transparent_30%)]
+      "
+    />
 
-      <main className="mx-auto flex min-h-screen max-w-xl flex-col px-5 py-10">
+    {/* Contenido */}
+    <div className="relative z-10">
 
-        <HomeCard />
+        <main className="mx-auto flex min-h-screen max-w-xl flex-col px-5 py-10">
 
-      </main>
+          <HomeCard />
 
+        </main>
 
-      <footer className="fixed bottom-4 right-10 flex">
-      
-        <a
-          href="https://github.com/betra0/que-nota-necesito"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="
-            flex items-center justify-center
-            rounded-lg
-            bg-gray-900
-            shadow-xl
-            shadow-black/100
-            px-2 py-2
-            text-sm text-gray-100
-            transition
-            hover:bg-gray-700
-            hover:shadow-xl
-          "
-        >
-         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="h-7 w-7 text-white"
-         >
-          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-        </svg>
-        </a>
-      </footer>
+        <div className='fixed bottom-4 right-10 flex'>
+
+          <button className='flex items-center justify-center
+              rounded-lg
+              bg-gray-900
+              shadow-xl
+              shadow-black/100
+              px-2 py-2
+              text-sm text-gray-100
+              transition
+              hover:bg-gray-700
+              hover:shadow-xl'></button>
+        </div>
+        <Footer/>
     </div>
+  </div>
   )
 }
 
